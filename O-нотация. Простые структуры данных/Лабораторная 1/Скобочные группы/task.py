52a2bc26709bbdabec2d2939dd031cb1521cc28c
kolyanich72 <kolyanich72@gmail.com>
2023-09-15 12:26:50 +0300

    qq11

--- O-нотация. Простые структуры данных/Лабораторная 1/Скобочные группы/task.py
+++ O-нотация. Простые структуры данных/Лабораторная 1/Скобочные группы/task.py
@@ -27,17 +27,18 @@ def check_brackets(brackets_row: str) -> bool:
     :return: True, если последовательность корректна, False в противном случае
     """
     q = Stack()
-
+    if len(brackets_row) == 0:
+        res = True
     for i in (brackets_row):
         if i == "(":
             res = q.push()
         if i == ")":
             res = q.pop_()
     return res
-    # TODO реализовать проверку скобочной группы
 
 
 if __name__ == '__main__':
+    print(check_brackets(""))  # True
     print(check_brackets("()()"))  # True
     print(check_brackets(")("))  # False
     print(check_brackets("()())("))  # False
