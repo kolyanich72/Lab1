b2f84c846aab034c85b31f30187042c0eac96214
kolyanich72 <kolyanich72@gmail.com>
2023-09-27 09:10:47 +0300

    L2

---
+++ Итеративные и рекурсивные алгоритмы/Лабораторная 2/Итеративный линейный поиск/task.py
@@ -0,0 +1,25 @@
+"""
+This module implements some functions based on linear search algo
+"""
+from typing import List
+
+
+def min_search(arr: List[int]) -> int:
+    """
+    Функция для поиска минимума в массиве
+    :param arr: Массив целых чисел
+    :return: Индекс первого вхождения элемента в массиве
+    """
+     # TODO реализовать итеративный линейный поиск
+    if len(arr) == 0:
+        raise ValueError
+
+    min_ = arr[0]
+    for i in range(1, len(arr)):
+        if arr[i] < arr[min_]:
+            min_ = i
+
+    return min_
+
+if __name__ == "__main__":
+    print(min_search([5, 3, 15, 20, 2, 25, 22, 2, 10, 2, 8]))
