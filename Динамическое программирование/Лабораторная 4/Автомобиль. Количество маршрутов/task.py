c76bbff1c1cdfb43a16fd42ca4050bfe1028426d
kolyanich72 <kolyanich72@gmail.com>
2023-09-29 09:08:17 +0300

    L2_3_41

--- Динамическое программирование/Лабораторная 4/Автомобиль. Количество маршрутов/task.py
+++ Динамическое программирование/Лабораторная 4/Автомобиль. Количество маршрутов/task.py
@@ -10,8 +10,8 @@ def car_paths(n: int, m: int) -> List[List[int]]:
 
     :return: Новую таблицу с посчитанным количеством маршрутов в каждую клетку
     """
-     # TODO решить задачу с помощью динамического программирования
-    table =[[0 for i in range(m)] for j in range(n)]
+
+    table = [[0 for i in range(m)] for j in range(n)]
 
     for i in range(n):
         table[i][0] = 1
@@ -21,12 +21,11 @@ def car_paths(n: int, m: int) -> List[List[int]]:
         for j in range(1, m):
             table[i][j] = table[i-1][j] + table[i][j-1] + table[i-1][j-1]
 
-
     return table
+
 
 if __name__ == '__main__':
     paths = car_paths(5, 5)
  #   paths = car_paths(4, 2)
     print(paths[-1][-1])  # 7
     print(paths)
-
